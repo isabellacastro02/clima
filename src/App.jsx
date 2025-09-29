51b51485b796c24a5e4eef2f880923e3fbf89ada
isabellacastro02 <isabella.mcastro02@gmail.com>
2025-09-29 10:56:46 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -21,17 +19,38 @@ function App() {
   ServiceWorkerRegistration('');
 
   try{
+    const API_KEY = 'bd5e378503939ddaee76f12ad7a97608'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid${API_KEY}&units=metric&lang=pt_br`;
+    const resposta = await fetch(url);
+
+    if(!resposta.ok){
+      throw new Error('Cidade não encontrada');
+    }
+
+    const dados = await resposta.json();
+    setClima(dados);
 
   } catch (error){
-
+      setErro(error.message);
+      setClima(null);
   } finally {
-
+      setCarregando(false);
   }
-}
+}; //fecha buscaClima()
+
+  const handleKeyPress = (e) => {
+    if (e.key == 'Enter'){
+      buscaClima();
+    }
+  };
 
   return (
     <>
-     
+      <div className="app-container">
+        <div className="content-wrapper">
+
+        </div>
+      </div>
     </>
   )
 }
